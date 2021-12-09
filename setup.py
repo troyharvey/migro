fae45f336850de9322d024a42c9ccd2c0ccb5158
Troy Harvey <troyharvey@gmail.com>
2021-12-08 23:23:32 -0500

    Add README long description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='migro',
-    version='0.0.2',
+    version='0.0.3',
     description='Redshift Data Warehouse migrations for dbt.',
     author='Troy Harvey',
     author_email='troyharvey@gmail.com',
-    license='MIT',
     url='https://github.com/troyharvey/migro',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
