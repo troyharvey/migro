1c51c5460b019ebd3e664a6b0f9785d79cd5ac23
Troy Harvey <troyharvey@gmail.com>
2021-12-12 00:09:36 -0500

    Add generate_password test

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,5 +1,6 @@
-from migro.migrations import MigrationRepository
+from migro import migrations
 import os
+import re
 
 
 class TestMigrations:
@@ -10,14 +11,27 @@ class TestMigrations:
             pass
 
     def teardown_method(self):
-        os.remove("./tests/demo.db")
+        try:
+            os.remove("./tests/demo.db")
+        except:
+            pass
 
-    def test_get_migration_files(self):
-        migrations = MigrationRepository()
-        migration_files = migrations._get_migration_files()
-        assert migration_files == []
+    def test_migrations_path_constant(self):
+        assert migrations.MIGRATION_FILE_PATH == './migrations'
 
-    def test_all(self):
-        migrations = MigrationRepository('sqlite_profile')
-        migrations = migrations.all()
-        assert migrations == []
+    def test_generate_password(self):
+        password = migrations.generate_password()
+        assert len(password) == 32
+        assert re.match('^[A-Za-z0-9$%@]{32}$', password)
+
+
+
+    # def test_get_migration_files(self):
+    #     migrations = MigrationRepository()
+    #     migration_files = migrations._get_migration_files()
+    #     assert migration_files == []
+
+    # def test_all(self):
+    #     migrations = MigrationRepository('sqlite_profile')
+    #     migrations = migrations.all()
+    #     assert migrations == []
