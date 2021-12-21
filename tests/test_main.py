b8247b9197e7901aadb1354534ee3ffb96c1500d
Troy Harvey <troyharvey@gmail.com>
2021-12-21 14:49:10 -0500

    Add click cli tests

---
+++ tests/test_main.py
@@ -0,0 +1,30 @@
+from click.testing import CliRunner
+from migro import main
+
+def test_cli():
+  runner = CliRunner()
+  result = runner.invoke(main.cli)
+  assert result.exit_code == 0
+  assert "Data Warehouse migrations" in result.output
+
+def test_make():
+  runner = CliRunner()
+  result = runner.invoke(main.make, "add_user_tharvey")
+  assert result.exit_code == 0
+  assert "Created:" in result.output
+  assert "_add_user_tharvey.sql" in result.output
+
+def test_up_pretend():
+    runner = CliRunner()
+    result = runner.invoke(main.up, ["--pretend", "--limit=1", "--dbt-profile=sqlite_profile"])
+    assert result.exit_code == 0
+    assert "Migrating:" in result.output
+    assert "add_user_tharvey.sql" in result.output
+
+def test_up():
+    runner = CliRunner()
+    result = runner.invoke(main.up, ["--dbt-profile=sqlite_profile"])
+    assert result.exit_code == 0
+    assert "Migrating:" in result.output
+    assert "add_user_tharvey.sql" in result.output
+    assert "Migrated:" in result.output
