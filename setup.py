13863188c1ab6fa8ad2f97a2498ce596b72cb7ef
Troy Harvey <t.harvey@carta.com>
2022-05-18 14:28:33 -0400

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='migro',
-    version='0.1.0',
+    version='0.2.0',
     description='Data Warehouse migrations for dbt.',
     author='Troy Harvey',
     author_email='troyharvey@gmail.com',
